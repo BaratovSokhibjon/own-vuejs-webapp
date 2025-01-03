f5159956ad70a6073a4a7babfeb9c0a626374074
Baratov Sokhibjon <sokhibjonbaratov@gmail.com>
2025-01-03 09:06:02 +0900

    feat: a form compenent that gets the information from the App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <navbar :pages="pages" :active-page="activePage" :nav-link-click="navLinkClick" />
-  <page-viewer v-if="pages.length > 0" :page="pages[activePage]" />
+  <!-- <page-viewer v-if="pages.length > 0" :page="pages[activePage]" /> -->
+  <create-page :page-created="pageCreated"></create-page>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
 import PageViewer from './components/PageViewer.vue';
+import CreatePage from './components/CreatePage.vue';
 
 export default {
   components: {
     Navbar,
-    PageViewer
+    PageViewer,
+    CreatePage
   },
   props: {
     pages: {
---
+++ src/components/CreatePage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+    <form action="">
+      <div class="my-3">
+        <label for="" class="form-label">
+          Page Title
+        </label>
+        <input type="text" class="form-control" :value="pageTitle" @input="(event) => pageTitle = event.target.value" />
+      </div>
+
+      <div class="mb-3">
+        <label for="" class="form-label">
+          Page content
+        </label>
+        <textarea type="text" class="form-control" rows="5"></textarea>
+      </div>
+
+      <div class="mb-3">
+        <button class="btn btn-primary" @click.prevent="pageCreated({pageTitle})">Create Page</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        pageTitle: {
+            type: String,
+            default: ""
+        },
+        pageCreated:{
+            type: Function,
+            required: true
+        }
+    },
+    data() {
+        return{
+            pageTitle: ''
+        }
+    },
+    methods: {
+        pageCreated(pageTitle) {
+            console.log(pageTitle)
+        }
+    }
+}
+</script>
